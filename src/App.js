d27856adca1ed3d374727e0cca1fdf3d78180d1b
ullaskishan <ullas1000@gmail.com>
2022-09-15 19:33:19 +0530

    initial code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import readmePath from "./README.md";
+import { marked } from "marked";
 function App() {
+  const [data, setData] = useState("");
+  useEffect(() => {
+    fetch(readmePath)
+      .then((response) => {
+        return response.text();
+      })
+      .then((text) => {
+        setData(marked(text));
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <article dangerouslySetInnerHTML={{ __html: data }} />
+    </React.Fragment>
   );
 }
 
---
+++ src/README.md
@@ -0,0 +1,3 @@
+## How to read `markdownfile` aka `md` file in ReactJS
+
+It is pretty much simple to read the md file using the NPM package 'mark'.
